040ad9c4296a1e6fdead8c1e72551d2d8869425d
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-23 14:56:50 +0900

    Learned props of Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text"
-//           onChange={() => console.log('changed!')}
-//         ></input>
-//       </>
-//     );
-//   }
-// }
+const App = () => {
+  const profiles = [
+    { id: 1 ,name: 'Taro', age: 10},
+    { id: 2, name: 'Hanako', age: 5 },
+    { id: 3, age: 15}
+  ]
+  return (
+    <>
+      {
+        profiles.map(p =>
+          <User key={p.id} name={p.name} age={p.age} />
+        )
+      }
+    </>
+  );
+}
 
-const App = () => (
-  <>
-    <Cat />
-    <Cat />
-    <Cat />
-  </>
+const User = (props) => (
+  <p>Hi!, I am {props.name}, and {props.age} years old!</p>
 );
 
-const Cat = () => (
-  <div>Meow!</div>
-);
+User.defaultProps = {
+  name: 'unknown'
+};
 
 export default App;
