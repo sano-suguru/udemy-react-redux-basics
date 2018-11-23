9669b61028f2a3112c353a455b449f10c3ddfdaa
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-23 16:00:19 +0900

    Implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-function App () {
+import Counter from './Counter';
+
+export default function App() {
   return (
-    <>
-      <Counter />
-    </>
+    <Counter />
   );
 }
-
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 0
-    }
-  }
-
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 });
-  }
-
-  render() {
-    return (
-      <>
-        <p>
-          count: { this.state.count }
-        </p>
-        <button onClick={this.handlePlusButton}>
-          increment
-        </button>
-        <button onClick={this.handleMinusButton}>
-          decrement
-        </button>
-      </>
-    );
-  }
-}
-
-export default App;
