5af6caa08758b62bfdcdcfd460d83af0983703cc
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-23 14:43:54 +0900

    Learned about class and functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <label htmlFor="bar">bar</label>
-        <input type="text"
-          onChange={() => console.log('changed!')}
-        ></input>
-      </>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text"
+//           onChange={() => console.log('changed!')}
+//         ></input>
+//       </>
+//     );
+//   }
+// }
+
+const App = () => (
+  <>
+    <Cat />
+    <Cat />
+    <Cat />
+  </>
+);
+
+const Cat = () => (
+  <div>Meow!</div>
+);
 
 export default App;
